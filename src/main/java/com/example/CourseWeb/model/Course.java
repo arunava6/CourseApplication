885553d32f14e8843a9ef0f18c8dec0dec254679
arunava6 <arunavasaharoy@gmail.com>
2025-12-27 11:47:56 +0530

    initial commit

--- src/main/java/com/example/CourseWeb/model/Course.java
+++ src/main/java/com/example/CourseWeb/model/Course.java
@@ -1,6 +1,7 @@
 package com.example.CourseWeb.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import java.time.LocalDate;
 
 
 @Entity
-@Table(name="courses")
+@Table(name = "courses")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,17 +19,26 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer courseId;
 
+    @NotBlank(message = "Course Name can't blank")
     @Column(nullable = false)
     String courseName;
 
+    @NotBlank(message = "Description cannot be blank")
+    @Size(min=20, max=200,message = "Description length should be more than 20 and less than 200")
     @Column(nullable = false)
     String courseDesc;
     String tags;
+
+    @Min(1)
+    @Max(2000)
     Integer duration;
 
+    @PastOrPresent
     @Column(nullable = false)
     LocalDate listedOn;
 
+    @Min(0)
+    @Max(200000)
     @Column(nullable = false)
     Double price;
 
--- src/main/java/com/example/CourseWeb/service/CourseService.java
+++ src/main/java/com/example/CourseWeb/service/CourseService.java
@@ -3,12 +3,15 @@ package com.example.CourseWeb.service;
 import com.example.CourseWeb.model.Course;
 import com.example.CourseWeb.repositories.CourseRepo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CourseService {
+
+    @Autowired
     CourseRepo repo;
 
     public List<Course> getAllCourses() {
